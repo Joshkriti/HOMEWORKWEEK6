86220bbb25ecafe21b55374c4b5b3885e91193df
Joshkriti <Komalkanji13@gmail.com>
2024-04-08 19:58:20 +0100

    my first push to demo

---
+++ Testdemo/src/question2/StaticMethodAndMainMethod.java
@@ -0,0 +1,12 @@
+package question2;
+
+public class StaticMethodAndMainMethod {
+    /* Question 2.5: Call the static method
+    into the Main method and run the programme. */
+
+    public static void main(String[] args) {
+        System.out.println("Thank You"); // Main method
+
+
+    }
+}
