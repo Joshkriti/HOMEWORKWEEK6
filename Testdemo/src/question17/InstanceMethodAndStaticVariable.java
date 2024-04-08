99f999bd12651933ac95298a4b486b0cd3038ca1
Joshkriti <Komalkanji13@gmail.com>
2024-04-08 22:52:33 +0100

    my first push to demo

---
+++ Testdemo/src/question17/InstanceMethodAndStaticVariable.java
@@ -0,0 +1,17 @@
+package question17;
+
+public class InstanceMethodAndStaticVariable {
+    // Question 17.3 Declare one instance method and call static variable in print statement
+
+    // Creating static variable
+    static String name = "United Kingdom";
+
+    public static void main(String[] args){
+        System.out.println(name); // access static variable without using object
+
+
+
+    }
+
+
+}
