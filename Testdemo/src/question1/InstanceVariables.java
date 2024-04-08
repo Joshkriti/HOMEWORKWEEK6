dbba918aa42a410b4e4e0f0c90f19f836a261bfe
Joshkriti <Komalkanji13@gmail.com>
2024-04-08 16:22:55 +0100

    my second push to demo

--- Testdemo/src/question1/InstanceVariables.java
+++ Testdemo/src/question1/InstanceVariables.java
@@ -3,6 +3,7 @@ package question1;
 public class InstanceVariables {
     public static void main(String[] args){
         System.out.println("Komal");
+        System.out.println("Kanji");
     }
 }
 
