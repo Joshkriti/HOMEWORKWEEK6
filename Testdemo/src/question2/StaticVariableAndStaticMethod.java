c41825c9e435e0dfe90112a670c1df61e23aba71
Joshkriti <Komalkanji13@gmail.com>
2024-04-08 19:39:06 +0100

    my first push to demo

---
+++ Testdemo/src/question2/StaticVariableAndStaticMethod.java
@@ -0,0 +1,14 @@
+package question2;
+
+public class StaticVariableAndStaticMethod {
+    /* Question 1.3: Call both static variables
+     into the static method inside the print statement */
+
+    static String name2 = "Komal Kanji"; // Static Variable
+
+    public static void main(String[] args){
+        System.out.println(name2); // access static variable without object
+
+    }
+
+}
