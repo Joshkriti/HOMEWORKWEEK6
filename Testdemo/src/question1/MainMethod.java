c81c2911ddebf14d1225eb4cd9536aea3ef3d9cf
Joshkriti <Komalkanji13@gmail.com>
2024-04-08 18:06:24 +0100

    my first push to demo

---
+++ Testdemo/src/question1/MainMethod.java
@@ -0,0 +1,9 @@
+package question1;
+
+public class MainMethod {
+    // Question 1.4: Declare the Main method.
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
