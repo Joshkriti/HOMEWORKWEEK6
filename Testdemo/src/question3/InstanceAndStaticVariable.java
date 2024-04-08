d1d8d8e3b40e5d6c01e6f833c42e19d53542200f
Joshkriti <Komalkanji13@gmail.com>
2024-04-08 20:11:01 +0100

    my first push to demo

---
+++ Testdemo/src/question3/InstanceAndStaticVariable.java
@@ -0,0 +1,16 @@
+package question3;
+
+public class InstanceAndStaticVariable {
+    // Question 3.1: Declare one instance and one static variable.
+
+    String name = "Hello!!";  // Instance variable
+
+    public static void main(String[] args){
+        InstanceAndStaticVariable I = new InstanceAndStaticVariable();
+        System.out.println(I.name); // access instance variable vie object
+
+
+    }
+
+}
+
