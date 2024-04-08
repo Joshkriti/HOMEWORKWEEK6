ec3484dbd69ba0d6c5b1eb4f420d86501951281b
Joshkriti <Komalkanji13@gmail.com>
2024-04-08 18:11:04 +0100

    my first push to demo

---
+++ Testdemo/src/question1/InstanceMethodAndMainMethod.java
@@ -0,0 +1,13 @@
+package question1;
+
+public class InstanceMethodAndMainMethod {
+    /* Question 1.5: Call the above instance method
+    into the Main method and run the programme. */
+
+    // Main method
+    public static void main(String[] args) {
+        System.out.println("Hey It's me Komal");
+    }
+
+
+}
