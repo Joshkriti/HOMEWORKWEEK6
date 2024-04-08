446a80383034e33a5dac0cd17392cd1a6432616c
Joshkriti <Komalkanji13@gmail.com>
2024-04-08 16:51:32 +0100

    my secong push to demo

--- Testdemo/src/question1/InstanceVariables1.java
+++ Testdemo/src/question1/InstanceVariables1.java
@@ -5,10 +5,13 @@ public class InstanceVariables1 {
 
 
     String name = "Komal"; // this is instance variables
+    String name1 = "Kanji"; // this is instance variables
 
     public static void main(String[] args){
         InstanceVariables1 v = new InstanceVariables1();
         System.out.println(v.name); // access instance variable via object
+        System.out.println(v.name1); // access instance variable via object
+
 
 
     }
