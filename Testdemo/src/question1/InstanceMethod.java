e48a7b1dae106a9fd2467545813198ff7c2f5067
Joshkriti <Komalkanji13@gmail.com>
2024-04-08 17:42:47 +0100

    my first push to demo

---
+++ Testdemo/src/question1/InstanceMethod.java
@@ -0,0 +1,20 @@
+package question1;
+
+public class InstanceMethod {
+    // Question 1.1: Declare one instance method.
+
+    public static void main(String[] args){
+        InstanceMethod m = new InstanceMethod();
+        m.name(); // access userDefined instance method to main method
+
+
+    }
+    // Instance Methods
+    public void  name() {
+        System.out.println("I am learning");
+    }
+
+}
+
+
+
