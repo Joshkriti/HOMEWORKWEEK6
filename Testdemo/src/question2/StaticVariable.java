18516d54cfe269890be3f689b970832b7d08226c
Joshkriti <Komalkanji13@gmail.com>
2024-04-08 19:18:52 +0100

    my second push to demo

--- Testdemo/src/question2/StaticVariable.java
+++ Testdemo/src/question2/StaticVariable.java
@@ -4,10 +4,10 @@ public class StaticVariable {
     // Question 2.1: Declare two static variables.
 
     static String name = "Komal"; // Static variable
-
+    static String name1 = "Kanji"; // Another Static variable
 
     public static void main(String[] args ){
         System.out.println(name); // access static without object
-
+        System.out.println(name1); //access static without object
     }
 }
