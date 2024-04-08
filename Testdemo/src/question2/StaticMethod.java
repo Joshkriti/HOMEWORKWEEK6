656a0628cd02efe3417adc1fbc2b0352639bce94
Joshkriti <Komalkanji13@gmail.com>
2024-04-08 19:30:54 +0100

    my first push to demo

---
+++ Testdemo/src/question2/StaticMethod.java
@@ -0,0 +1,14 @@
+package question2;
+
+public class StaticMethod {
+    // Question 1.2: Declare one static method.
+
+    public static void main(String [] args) {
+    name1();
+    }
+
+    // Instance method
+    public static void name1() {
+        System.out.println("You're Welcome!!");
+    }
+}
