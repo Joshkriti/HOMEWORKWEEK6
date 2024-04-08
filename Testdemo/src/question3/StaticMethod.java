7ed484829f8ce374a9db71ec0d225c0198dac9d7
Joshkriti <Komalkanji13@gmail.com>
2024-04-08 20:30:14 +0100

    my first push to demo

---
+++ Testdemo/src/question3/StaticMethod.java
@@ -0,0 +1,15 @@
+package question3;
+
+public class StaticMethod {
+    // Question 3.3: Declare one static method.
+
+    public static void main(String[] args){
+        name3(); // Static method without writing object
+
+    }
+    // Static method
+    public static void name3(){
+        /* Print out for static variable */
+        System.out.println("Komal, You are doing good so far.");
+    }
+}
