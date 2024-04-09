a441586db6fc60fcbddb555c4aea4235d0d7abe7
Joshkriti <Komalkanji13@gmail.com>
2024-04-09 19:27:35 +0100

    my first push to demo

---
+++ Testdemo/src/question20/StaticVariable.java
@@ -0,0 +1,17 @@
+package question20;
+
+public class StaticVariable {
+    // question 20.1: Declare your Spain as static variable.
+
+    static String country = "Spain"; // Creating spain as static variable
+
+
+    public static void main(String[] args) {
+        System.out.println(country);  // defining static variable into main method
+
+    }
+
+
+
+
+}
