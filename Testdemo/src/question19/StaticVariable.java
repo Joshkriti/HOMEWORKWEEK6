c40313ded274d1d3efd61eb4bfc39275fa3e9550
Joshkriti <Komalkanji13@gmail.com>
2024-04-09 15:39:00 +0100

    my first push to demo

---
+++ Testdemo/src/question19/StaticVariable.java
@@ -0,0 +1,13 @@
+package question19;
+
+public class StaticVariable {
+    // Question 19.2: Declare one of your group member names as static variable.
+
+    static String name = "Deval Patel"; // Creating static variable
+
+    public static void main(String[] args) {
+        System.out.println(name);  // access to static variable without using object
+
+    }
+
+}
