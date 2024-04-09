c5696b3fc9d9ff0cf09d1de596ac86fccf7456cc
Joshkriti <Komalkanji13@gmail.com>
2024-04-09 20:48:39 +0100

    my first push to demo

---
+++ Testdemo/src/Week5Homework/Question16.java
@@ -0,0 +1,17 @@
+package Week5Homework;
+
+public class Question16 {
+    /* Question 16: Write program to display
+    Test Data: ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+     Expected Output 2.138888888888889    */
+
+    public static void main(String[] args) {
+        double X = (25.5 * 3.5 - 3.5 * 3.5);
+        double Y = (40.5 - 4.5);
+        double Z = X / Y;
+
+        System.out.println(Z);
+
+    }
+
+}
