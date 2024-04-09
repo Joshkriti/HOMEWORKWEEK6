f203cb287fdd3c9bd77a1e71b671a9325baf639c
Joshkriti <Komalkanji13@gmail.com>
2024-04-09 15:35:40 +0100

    my first push to demo

---
+++ Testdemo/src/question19/InstanceVariable.java
@@ -0,0 +1,15 @@
+package question19;
+
+public class InstanceVariable {
+    // question 19.1: Declare one of your group member names as instance variable.
+
+    String name = "Vaishali"; // Creating instance variable
+
+    public static void main(String[] args) {
+        InstanceVariable I = new InstanceVariable();
+        System.out.println(I.name); // access instance variable vie object
+
+    }
+
+
+}
