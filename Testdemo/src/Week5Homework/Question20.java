5da6aaa3e78d15918cd1408b2fd2dc5be5bb68d0
Joshkriti <Komalkanji13@gmail.com>
2024-04-09 20:52:50 +0100

    my first push to demo

---
+++ Testdemo/src/Week5Homework/Question20.java
@@ -0,0 +1,15 @@
+package Week5Homework;
+
+public class Question20 {
+    // Question 20: Write a program to add 5 and 6.2, and print in console.
+
+    public static void main(String[] args) {
+
+        int X = 5;
+        double Y = 6.2;
+        double Z = X + Y;
+
+        System.out.println(Z);
+    }
+
+}
