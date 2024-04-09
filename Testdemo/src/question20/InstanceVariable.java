77145b6a9f7fb67eff0e76f8720efd28be55e8dc
Joshkriti <Komalkanji13@gmail.com>
2024-04-09 19:35:47 +0100

    my first push to demo

---
+++ Testdemo/src/question20/InstanceVariable.java
@@ -0,0 +1,13 @@
+package question20;
+
+public class InstanceVariable {
+    // Question 20.2: Declare your United Kingdom as instance variable.
+
+    String country = "United Kingdom"; // Creating instance method as country
+
+    public static void main(String[] args) {
+        InstanceVariable I = new InstanceVariable();
+        System.out.println(I.country); // access instance variable via object
+        }
+    }
+
