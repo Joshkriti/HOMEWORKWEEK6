4a82dba279cd5ddd3bd9d6a586273f01fd6d4445
Joshkriti <Komalkanji13@gmail.com>
2024-04-09 22:29:40 +0100

    my first push to demo

---
+++ Testdemo/src/homeworkweek6/Question15.java
@@ -0,0 +1,14 @@
+package homeworkweek6;
+
+public class Question15 {
+    /* 15. Write a Java program to convert a given string into lowercase.
+ Sample Input: THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+ Output: the quick brown fox jumps over the lazy dog.*/
+
+    static String text ="THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.";
+
+    public static void main(String[] args) {
+        System.out.println(text.toUpperCase());
+        System.out.println(text.toLowerCase());
+    }
+}
