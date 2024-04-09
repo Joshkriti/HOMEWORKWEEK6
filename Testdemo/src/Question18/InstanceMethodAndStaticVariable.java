2bf260990f1c43d3809857320823d572edda0d9f
Joshkriti <Komalkanji13@gmail.com>
2024-04-09 19:50:13 +0100

    my first push to demo

--- Testdemo/src/Question18/InstanceMethodAndStaticVariable.java
+++ Testdemo/src/Question18/InstanceMethodAndStaticVariable.java
@@ -3,5 +3,17 @@ package Question18;
 public class InstanceMethodAndStaticVariable {
     // Question 18.3 Declare one instance method name borough() and call Static variable
 
-    
+    static String name = "Country:";  // Creating static variable
+
+    public static void main(String[] args) {
+        System.out.println(name); // access to static variable without using object
+        InstanceMethodAndStaticVariable I = new InstanceMethodAndStaticVariable();
+        I.borough(); // access to instance method vie object
+    }
+
+
+    // Instance method
+    public void borough() {
+        System.out.println("London");
+    }
 }
