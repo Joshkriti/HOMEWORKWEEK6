5dbe5bde047a38c142e5e7f49ca3fd85e5b81877
Joshkriti <Komalkanji13@gmail.com>
2024-04-09 22:06:40 +0100

    my first push to demo

--- Testdemo/src/homeworkweek6/Question9.java
+++ Testdemo/src/homeworkweek6/Question9.java
@@ -4,7 +4,7 @@ public class Question9 {
     // Question 9: Write a program to convert the upper case to lower case.
 
    static String text = "Hello World";
-   static String text1 ="My nake is Komal.";
+   static String text1 ="My name is Komal.";
 
     public static void main(String[] args) {
         System.out.println(text.toUpperCase());
