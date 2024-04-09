cd990430590d7c4bb3e8318a27aad0de335ed943
Joshkriti <Komalkanji13@gmail.com>
2024-04-09 21:45:14 +0100

    my first push to demo

---
+++ Testdemo/src/homeworkweek6/Question6.java
@@ -0,0 +1,18 @@
+package homeworkweek6;
+
+public class Question6 {
+    /* Question 6: Write a program to enter any radius value of the circle and find out the area. (Formula
+of Area A=PI*r*r). */
+
+
+    public static void main(String[] args) {
+        float A = 3.14F;
+        int B = 4;
+        int C = 4;
+        float D = A * B * C;
+
+        System.out.println(D);
+    }
+
+
+}
