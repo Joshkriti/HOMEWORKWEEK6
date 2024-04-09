030b7ed3319aed3c43f5c18b84ce88828fd6cc8d
Joshkriti <Komalkanji13@gmail.com>
2024-04-09 20:43:40 +0100

    my first push to demo

---
+++ Testdemo/src/Week5Homework/Question11.java
@@ -0,0 +1,29 @@
+package Week5Homework;
+
+public class Question11 {
+    /* Question 11: Write a Java program that takes a number as input and prints
+    its multiplication table upto 10.
+    Expected Output :
+    8 x 1 = 8
+    8 x 2 = 16
+    ......
+    8 x 10 = 80     */
+
+    public static void main(String[] args) {
+        int A = 8;
+        int B = 1;
+        int C = A * B;
+
+        int D = 8;
+        int E = 2;
+        int F = D * E;
+
+
+        System.out.println(C);
+        System.out.println(F);
+    }
+
+
+
+
+}
