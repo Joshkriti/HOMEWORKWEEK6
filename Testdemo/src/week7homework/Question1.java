43dc6a8630e071473d46127a9cdd313330082182
Joshkriti <Komalkanji13@gmail.com>
2024-04-17 18:58:14 +0100

    my first push to demo

---
+++ Testdemo/src/week7homework/Question1.java
@@ -0,0 +1,23 @@
+package week7homework;
+
+public class Question1 {
+    /* Question 1: Write a java program that tells us that Input number is odd or even? HINT: use ternary
+operator (? :) */
+
+    public static void main(String[] args){
+        int number = 11 ;
+
+        /* Writing IF statement to find out if the above number is odd or even */
+       if (number % 2 == 0) {
+           System.out.println(number + " is even.");
+       } else {
+           System.out.println(number + " is odd.");
+
+       }
+
+
+
+    }
+
+
+}
