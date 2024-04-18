6e58c737bccd4df75bf34e619881d266918d4a32
Joshkriti <Komalkanji13@gmail.com>
2024-04-18 23:08:34 +0100

    my first push to demo

--- Testdemo/src/homeworkweek6/Question5.java
+++ Testdemo/src/homeworkweek6/Question5.java
@@ -5,5 +5,30 @@ public class Question5 {
 methods all with parameters and use string concatenation methods. (Note: Two static
 and Two instance methods.) */
 
+    public static void main(String[] args) {
+        int result=calculator(5,6); // 11
+        int result1=calculator1(5,6);
+        int result2=calculator2(5,6);
+        int result3=calculator3(10, 2);
+
+        System.out.println(result); //one way
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+
+
+    }
+    private static int calculator(int x, int y) {
+        return x+y;
+    }
+    private static int calculator1(int a, int b){
+        return a-b;
+    }
+   private static int calculator2(int k, int l){
+        return k*l;
+   }
+   private static int calculator3(int c, int d){
+        return c/d;
+   }
 
 }
