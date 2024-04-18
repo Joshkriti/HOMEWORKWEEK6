189ca4399f7ef5effd39421cd0f702ab09b60f41
Joshkriti <Komalkanji13@gmail.com>
2024-04-18 19:57:00 +0100

    my first push to demo

--- Testdemo/src/week7homework/Question11.java
+++ Testdemo/src/week7homework/Question11.java
@@ -1,19 +1,36 @@
 package week7homework;
 
+import java.util.Scanner;
+
 public class Question11 {
     /* Question 11. Input any alphabet from “A" to “F” and print their city name accordingly (use if else) ifany
 other alphabet should be invalid entry. */
 
     public static void main(String[] args){
-       String city = "A";
-       if (city == "A")
-           System.out.println("aagra");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter character between A to F:");
+        String city = sc.nextLine();
+
+
+       if (city.equals("A")){
+           System.out.println("aagra");}
        else if (city.equals("B")) {
            System.out.println("Bhavnagar");
-       } else if (city == "C"){
+       } else if (city.equals("C")){
            System.out.println("Chotila");
-       }
+       } else if (city.equals("D")){
+           System.out.println("Delhi");
+       } else if (city.equals("E")){
+           System.out.println("Edinburgh ");
+       } else if (city.equals("F")) {
+           System.out.println("Florida");
+       } else
+           System.out.println("INVALID DATA");
+
+        }
+
 
 
     }
-}
+
