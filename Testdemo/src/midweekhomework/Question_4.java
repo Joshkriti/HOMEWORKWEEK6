d6e923b899655f80be640d5b14fcaa4f19861047
Joshkriti <Komalkanji13@gmail.com>
2024-04-19 20:23:48 +0100

    my first push to demo

---
+++ Testdemo/src/midweekhomework/Question_4.java
@@ -0,0 +1,19 @@
+package midweekhomework;
+
+public class Question_4 {
+
+    // Question 4: Take values of length and breadth of a rectangle from user and check if it is square or not.
+
+    public static void main(String[] args) {
+         int length = 4;
+         int breadth = 6;
+
+         if (length == breadth){
+             System.out.println("Square");
+         } else {
+             System.out.println("Not a square");
+         }
+    }
+
+
+}
---
+++ Testdemo/src/midweekhomework/Question_5.java
@@ -0,0 +1,21 @@
+package midweekhomework;
+
+public class Question_5 {
+
+    // Question 5: 5. Take two int values from user and print greatest among them.
+
+    public static void main(String[] args) {
+
+        int number = 25;
+        int number1 = 25;
+
+        if (number == number1) {
+            System.out.println("Both are equal");
+        } else if (number > number1){
+            System.out.println(number + " is greater");
+        }else {
+            System.out.println(number1 + " is greater");
+
+        }
+    }
+}
