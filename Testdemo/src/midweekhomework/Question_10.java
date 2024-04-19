f88ed2f880e6ece30600503964aaa31833b6bffd
Joshkriti <Komalkanji13@gmail.com>
2024-04-19 21:50:45 +0100

    my first push to demo

---
+++ Testdemo/src/midweekhomework/Question_10.java
@@ -0,0 +1,15 @@
+package midweekhomework;
+
+public class Question_10 {
+
+    //Question 10. Write a program to print the sum of two numbers entered by user by defining your
+    //own method.
+
+    public static void main(String[] args) {
+        int number = 50;
+        int number1 = 70;
+        int number3 = number + number1;
+
+        System.out.println(number3);
+    }
+}
---
+++ Testdemo/src/midweekhomework/Question_11.java
@@ -0,0 +1,13 @@
+package midweekhomework;
+
+public class Question_11 {
+
+    // 11. Write a program to print a string entered by user.
+
+    String name = "I am learning java programming language.";
+
+    public static void main(String[] args) {
+        Question_11 M = new Question_11();
+        System.out.println(M.name);
+    }
+}
